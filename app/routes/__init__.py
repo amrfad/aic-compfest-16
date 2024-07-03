b5d1bdef8fddebcd731ec41ad657cdb0fb105400
Amr Fadhilah A. <amr.fadhilah.tif423@polban.ac.id>
2024-07-03 18:14:47 +0700

    Protopype dashboard

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
-from .main import main
-from .auth import auth
-# from .dashboard import dashboard
-# from .chat import chat
-# from .learning_path import learning_path
 
 def init_app(app):
+    from .main import main
+    from .auth import auth
+    from .dashboard import dashboard
+    # from .chat import chat
+    # from .learning_path import learning_path
+
     app.register_blueprint(main)
     app.register_blueprint(auth)
-    # app.register_blueprint(dashboard)
+    app.register_blueprint(dashboard)
     # app.register_blueprint(chat)
     # app.register_blueprint(learning_path)
