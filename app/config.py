8cce1096af3b97012b5b7588e79e7a3bd20f2558
Amr Fadhilah A. <amr.fadhilah.tif423@polban.ac.id>
2024-07-05 22:51:27 +0700

    change config and requirements

--- app/config.py
+++ app/config.py
@@ -1,14 +1,14 @@
-# import os
-# from dotenv import load_dotenv
-
-# # Load environment variables from a .env file
-# load_dotenv()
 import os
 
-username = os.environ.get('DB_USER')
-password = os.environ.get('DB_PASS')
-database = os.environ.get('DB_NAME')
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@127.0.0.1:5432/{database}"
+    ### Untuk Lokal
+    from dotenv import load_dotenv
+
+    # Load environment variables from a .env file
+    load_dotenv()
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}/{os.getenv('DATABASE_NAME')}"
+
+    ### Untuk Deployment
+    # SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@127.0.0.1:5432/{os.environ.get('DB_NAME')}"
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
